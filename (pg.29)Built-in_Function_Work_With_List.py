475c2a447d8ca78f9464752a29c2945fc0cdfc3f
Pasuuu-Yahoo <pasuthorn1406@gmail.com>
2024-08-04 22:30:52 +0700

    Lecture06

---
+++ (pg.29)Built-in_Function_Work_With_List.py
@@ -0,0 +1,33 @@
+numbers = [4, 2, 9, 1, 5, 6,]
+
+lenth = len(numbers)
+print(f"{lenth}")
+
+total_sum = sum(numbers)
+print(f"{total_sum}")
+
+max_value = max(numbers)
+print(f"{max_value}")
+
+min_value = min(numbers)
+print(f"{min_value}")
+
+sorted_numbers = sorted(numbers)
+print(f"{sorted_numbers}")
+
+bool_list = [False, True, False]
+any_true = any(bool_list)
+print(f"{any_true}")
+
+all_true = all(bool_list)
+print(f"{all_true}")
+
+string = "Hello"
+char_list = list(string)
+print(f"{char_list}")
+
+reversed_numbers = list(reversed(numbers))
+print(f"{reversed_numbers}")
+
+enumerated_numbers = list(enumerate(numbers))
+print(f"{enumerated_numbers}")
