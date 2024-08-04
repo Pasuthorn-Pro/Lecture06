778de1e4a27dfedd2a45e2bb80e8b5ca78f522b9
Pasuuu-Yahoo <pasuthorn1406@gmail.com>
2024-08-04 22:42:58 +0700

    Lecture06

--- BARISTA_PAY.py
+++ BARISTA_PAY.py
@@ -11,7 +11,7 @@ def main():
     pay_rate = float(input("Enter the hourly pay rate: "))
 
     for index in range(NUM_EMPLOYEES):
-        gross_pay = hours[index] + pay_rate
+        gross_pay = hours[index] * pay_rate
         print("Gross pay for employee ", index + 1, ": $", \
               format(gross_pay, ",.2f"), sep="")
         
