06c1dac1c906a422ac54e815fdf5fb4dc136098b
Pasuuu-Yahoo <pasuthorn1406@gmail.com>
2024-08-04 19:21:59 +0700

    Lecture06

--- (pg.20)List_method.py
+++ (pg.20)List_method.py
@@ -5,3 +5,18 @@ heros.insert(0, h2[0])
 print(heros.index("Thor"))
 heros.insert(heros.index("Thor"), h2[1])
 print(heros)
+heros.remove("Superman")
+heros.append("Ant Man")
+print(heros)
+heros.sort()
+print(heros)
+heros.reverse()
+print(heros)
+newheros = heros
+newheros[0] = "Wonder Woman"
+print(heros)
+copyheros = [] + heros
+print(copyheros)
+copyheros[0] = "Human"
+print(heros)
+print(copyheros)
