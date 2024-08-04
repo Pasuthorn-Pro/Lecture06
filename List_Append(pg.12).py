8d955e05ba975897140715ab680d94143b58420e
Pasuuu-Yahoo <pasuthorn1406@gmail.com>
2024-08-04 14:40:52 +0700

    Lecture06

---
+++ List_Append(pg.12).py
@@ -0,0 +1,5 @@
+fruits = ["apple", "banana", "cherry"]
+more_fruit = ["mango", "pineapple"]
+for fruit in more_fruit:
+    fruits.append(fruit)
+print(f"Fruits after append: {fruits}")
---
+++ List_Modify_List(pg.09).py
@@ -0,0 +1,4 @@
+shapes = ["circle", "square", "triangle", "ractangle", "hexagon"]
+shapes[1] = "eclipes"
+shapes[3] = "pentagon"
+print(f"Modify shapes: {shapes}")
